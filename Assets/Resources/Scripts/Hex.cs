86184d25614d63d7d035e819df14639ffab4870b
PeachTreeOath <peachtreeoath@gmail.com>
2016-08-24 23:29:08 -0600

    Grid generation

---
+++ Assets/Resources/Scripts/Hex.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hex : MonoBehaviour
+{
+
+    public int row;
+    public int col;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Resources/Scripts/MapGenerator.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    private GameObject hexPrefab;
+    private Transform parentTransform;
+
+    // Use this for initialization
+    void Start()
+    {
+        hexPrefab = Resources.Load<GameObject>("Prefabs/Hex");
+        parentTransform = GameObject.Find("Terrain").transform;
+
+        GenerateHexGrid(5, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void GenerateHexGrid(int row, int col)
+    {
+        float width = hexPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
+        float height = hexPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
+        float startX = -(row * width) / 2;
+        float startY = (col * height) / 2;
+                
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                Hex hex = Instantiate<GameObject>(hexPrefab).GetComponent<Hex>();
+                hex.transform.SetParent(parentTransform);
+                hex.row = i;
+                hex.col = j;
+                float xOffset = startX + width * j * 0.75f;
+                float yOffset = startY - height * i * 0.8f;
+                if(j % 2 == 1)
+                {
+                    yOffset -= height * 0.4f;
+                }
+                hex.transform.position = new Vector2(xOffset, yOffset);
+            }
+        }
+    }
+}
