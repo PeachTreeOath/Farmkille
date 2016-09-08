d480912ee42c24f863f4628ade26e3db4cc24d6d
PeachTreeOath <peachtreeoath@gmail.com>
2016-09-08 00:00:09 -0600

    Sprite clean up

---
+++ Assets/Resources/Scripts/AbstractTileAffector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbstractTileEffector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
