a656fa389a28d84b20b821c8a2bd71084ebee30f
PeachTreeOath <peachtreeoath@gmail.com>
2016-09-17 02:46:23 -0400

    Flowchart between scenes

--- Assets/Resources/Scripts/GlobalInfo.cs
+++ Assets/Resources/Scripts/GlobalInfo.cs
@@ -3,22 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-public class GlobalInfo : MonoBehaviour {
+public class GlobalInfo : MonoBehaviour
+{
 
     public static GlobalInfo instance;
 
     public int year = 1;
     public int gold = 0;
+    public int exp = 0;
     public int stamina = 5;
+
     public List<WorkerType> workers;
 
     void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
             instance = this;
         }
-        else if(instance != this)
+        else if (instance != this)
         {
             Destroy(gameObject);
         }
---
+++ Assets/Resources/Scripts/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+    private ScreenFader screenFader;
+
+    // Use this for initialization
+    void Start () {
+        screenFader = GameObject.Find("Canvas/ScreenFader").GetComponent<ScreenFader>();
+        screenFader.FadeIn();
+    }
+	
+    public void GoToGame()
+    {
+        screenFader.FadeOut("Game");
+    }
+
+    public void GoToBuildings()
+    {
+        SceneManager.LoadScene("Buildings");
+    }
+}
