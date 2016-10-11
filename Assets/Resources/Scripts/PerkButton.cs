9c62d72d1ec98edae5f75cb54fa63a8e867080d9
PeachTreeOath <peachtreeoath@gmail.com>
2016-10-11 00:00:07 -0600

    Hover over upgrades

---
+++ Assets/Resources/Scripts/PerkButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PerkButton : MonoBehaviour {
+
+    private Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+        canvas = GetComponentInChildren<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseEnter()
+    {
+        canvas.enabled = true;
+    }
+
+    void OnMouseExit()
+    {
+        canvas.enabled = false;
+    }
+}
