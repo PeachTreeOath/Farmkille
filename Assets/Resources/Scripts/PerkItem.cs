0288d70db24c361a43240a9773db61590b1ac75a
PeachTreeOath <peachtreeoath@gmail.com>
2016-10-29 13:48:22 -0400

    Perkitem code

---
+++ Assets/Resources/Scripts/PerkItem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(BoxCollider2D))]
+public class PerkItem : MonoBehaviour {
+
+    public SpriteRenderer sprite;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+    
+    void OnMouseDown()
+    {
+        sprite.enabled = !sprite.enabled;
+    }
+}
